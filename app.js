4e5b79a2a1d60e1d70b51feddcc7a5266e502d6a
Aryan <chaudharyaryan3456@gmail.com>
2025-06-26 17:25:29 +0530

    created models

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -15,6 +16,20 @@ main()
 async function main(){
     await mongoose.connect(MONGO_URL);
 }
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My new villa",
+        description:"By the beach",
+        price:1200,
+        location:"Calangute, Goa",
+        country:"India",
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("Successful testing");
+});
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
