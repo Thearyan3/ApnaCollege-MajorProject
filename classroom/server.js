d2f388716f9283d3abb2586d63f0187fd9d13092
Aryan <chaudharyaryan3456@gmail.com>
2025-07-22 23:26:28 +0530

    learning Routes

--- classroom/server.js
+++ classroom/server.js
@@ -14,3 +14,22 @@ app.listen(3000, (req, res) => {
     console.log("Server is listening to the port 3000");
 });
 
+//Index - users
+router.get("/", (req, res) => {
+    res.send("GET for users");
+});
+
+//Show - users
+router.get("/:id", (req, res) => { 
+    res.send("GET for users id");
+});
+
+//POST - users
+router.post("/", (req, res) => {
+    res.send("POST for users");
+});
+
+//Delete - users
+router.delete("/:id", (req, res) => {
+    res.send("Delete for users id");
+});
