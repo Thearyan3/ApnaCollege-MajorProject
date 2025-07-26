6af588038c476f637d441165d107e4c2b29a41f3
Aryan <chaudharyaryan3456@gmail.com>
2025-07-26 21:39:39 +0530

    Learning owner

--- routes/listing.js
+++ routes/listing.js
@@ -39,11 +39,12 @@ router.get("/new", isLoggedin, (req, res) => {
 //krega jo use nhi milegi.
 router.get("/:id", wrapAsync(async (req, res) => {// Ye ek async function hoga jisme request aur response ayega
     let { id } = req.params; //aur jaise hi request /:id pe ayegi to ham use phle req.params se extract krenge aur {id} me store krenge. 
-    const listing = await Listing.findById(id).populate("reviews");//ab isi extracted id ki help se ham listing ke data ko find krenge aur isko listing variable ke andar store kradenge. 
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");//ab isi extracted id ki help se ham listing ke data ko find krenge aur isko listing variable ke andar store kradenge. 
     if(!listing){
         req.flash("error", "Listing you requested for does not exist");
         return res.redirect("/listings");
     }
+    console.log(listing);
     res.render("listings/show.ejs", { listing });//jo data mila h use show.ejs ko pass krdenge aur show.ejs ko render kr denge is route pe.
     //Ye route isliye create kiya gya h taki 8th step wali listings me create kiye hue links par click krke jo data show hoga vo isi route ke base par hoga.
     //Aur ye route bhi "/listings/:id" pe jo get request aayegi us specific id ke data ko return krega.
