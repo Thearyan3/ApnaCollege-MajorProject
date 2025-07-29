871113786305ea51f65a0d351ccf8ead88a73edf
Aryan <chaudharyaryan3456@gmail.com>
2025-07-29 21:41:33 +0530

    adding authorization

--- middleware.js
+++ middleware.js
@@ -34,7 +34,7 @@ module.exports.isreviewAuthor = async(req, res, next) => {
     let {id, reviewId} = req.params;
     let review = await Review.findById(reviewId);
     if(!review.author.equals(res.locals.currUser._id)){
-    req.flash("error", "You are not the Author");
+    req.flash("error", "You are not the Author of this review");
     return res.redirect(`/listings/${id}`);
     }
 
