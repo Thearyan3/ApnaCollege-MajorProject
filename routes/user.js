57b466b6294d8d3804a5284d18203c54d1e76d40
Aryan <chaudharyaryan3456@gmail.com>
2025-07-30 22:26:53 +0530

    rating.css

--- routes/user.js
+++ routes/user.js
@@ -7,20 +7,33 @@ const { saveredirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/users.js");
 
-router.get("/signup", userController.renderSignupForm);
+router.route("/signup")
+.get(userController.renderSignupForm)
+.post(wrapAsync(userController.signup));
 
-router.post("/signup", wrapAsync(userController.signup));
-
-router.get("/login", userController.renderLoginForm);
-
-router.post(
-    "/login",
-    saveredirectUrl,
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(saveredirectUrl,
     passport.authenticate("local", {
      failureRedirect: "/login",
      failureFlash: true 
     }),
     userController.login);
+
+// router.get("/signup", userController.renderSignupForm);
+
+// router.post("/signup", wrapAsync(userController.signup));
+
+// router.get("/login", userController.renderLoginForm);
+
+// router.post(
+//     "/login",
+//     saveredirectUrl,
+//     passport.authenticate("local", {
+//      failureRedirect: "/login",
+//      failureFlash: true 
+//     }),
+//     userController.login);
 
 router.get("/logout", userController.logout);
 
