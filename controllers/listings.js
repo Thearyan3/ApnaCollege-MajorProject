22339b3ea17123a2a924749570fc012ef89a9c34
Aryan <chaudharyaryan3456@gmail.com>
2025-08-06 20:31:00 +0530

    learning

--- controllers/listings.js
+++ controllers/listings.js
@@ -39,9 +39,10 @@ module.exports.createListing = async (req, res, next) => {
     // }
     let url = req.file.path;
     let filename = req.file.filename;
-    console.log(url, "..", filename);
+
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
     await newListing.save();//Ab jo data hamne create krke add kr liya h, use database me insert krne ke liye save() method ka use krenge. 
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");//Aur jaise hi add button par click krenge, vaise hi sara data ek listing me insert hokar vo listing database
