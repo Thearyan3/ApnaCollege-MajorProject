7b52590812dc52e40b03a4fb1c75429a0ea4ef4f
Aryan <chaudharyaryan3456@gmail.com>
2025-08-07 19:55:22 +0530

    learning

--- controllers/listings.js
+++ controllers/listings.js
@@ -57,7 +57,7 @@ module.exports.renderEditForm = async (req, res) => {
         return res.redirect("/listings");
     }
     let originalImageUrl = listing.image.url;
-    originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
     res.render("listings/edit.ejs", { listing, originalImageUrl });//ab yehi listing edit.ejs ko pass krdi aur edit.ejs render kr diya is route pe.
 }
 
