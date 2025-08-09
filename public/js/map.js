fe86b7ba02957a81f5fb90fc637917bb4cb87393
Aryan <chaudharyaryan3456@gmail.com>
2025-08-09 22:50:01 +0530

    added geocoding

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,10 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: [77.2088, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
+
+const marker1 = new mapboxgl.Marker()
+    .setLngLat([12.554729, 55.70651])
+    .addTo(map);
